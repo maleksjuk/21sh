a6d0633c4547e7475ac5cd9c94ac15daac937e6e
Oskar Banshee <obanshee@mi-r3.msk.21-school.ru>
2020-09-20 18:33:05 +0300

    test

--- test/test.c
+++ test/test.c
@@ -3,6 +3,8 @@
 # include <term.h>
 # include <curses.h>
 #include <unistd.h>
+#include <stdlib.h>
+# include <fcntl.h>
 
 # define KEY_R "\x1b\x5b\x43\x0\x0\x0"
 # define KEY_L "\x1b\x5b\x44\x0\x0\x0"
@@ -48,9 +50,11 @@ int main(void){
     static struct termios oldt, newt;
     char line[256];
     int i = 0;
+    int j = 0;
     int flag = 0;
 
     char buff[256];
+    char tmp[256];
 
 
     line[255] = '\0';
@@ -59,14 +63,17 @@ int main(void){
     tcgetattr( STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHOCTL);
-    // newt.c_oflag &= ~(ONLCR);
-    // newt.c_cc[VERASE] = '\b';
+    newt.c_cc[VMIN] = 1;
 
     tcsetattr( STDIN_FILENO, TCSANOW, &newt);
 
+    int fd = open("/dev/tty", O_RDONLY);
+    read(fd, &c, 1);
 
-    while((c = getchar()) != '!')
+    // while((c = getchar()) != '!')
+    while(c != '!')
     {
+        
         line[i] = c;
         i++;
         if (i == 255)
@@ -75,6 +82,9 @@ int main(void){
         escape[0] = escape[1];
         escape[1] = escape[2];
         escape[2] = c;
+
+        if (ft_isprint(c))
+            buff[j++] = c;
 
 
         // if (ft_strnequ(escape, KEY_L, 3))
@@ -91,35 +101,18 @@ int main(void){
 
         if (c == 127)
         {
-            // printf("escape: %d..%d..%d\n", escape[0], escape[1], escape[2]);
-            write(1, "\b", 1);
-            // putchar(27);
-            // putchar(91);
-            // putchar(68);
-            // putchar(' ');
-            // putchar(27);
-            // putchar(91);
-            // putchar(68);
-            // write(1, &c, 1);
-            // i = -1;
-            // printf("\033[33mDEL: ");
-            // while (newt.c_cc[++i])
-            //     printf("%d  ", newt.c_cc[i]);
-            // printf("\033[0m\n");
+            write(1, "\b \b", 3);
+            buff[--j] = '\0';
         }
 
-        // if (c == 27)
-        //     flag = 1;
-        // else if (flag == 1 && c == 91)
-        //     flag = 2;
-        // else if (flag == 2 && c == 67)
-        // {
-        //     write(1, "#", 1);
-        //     flag = 0;
-        // }
+        read(fd, &c, 1);
+
     }
 
-    printf("catch: %c\n", c);
+    // read(0, buff, 254);
+
+    printf("catch: |%c|\n", c);
+    printf("buff: |%s|\n", buff);
 
     i = 0;
     while (line[i])
@@ -135,15 +128,11 @@ int main(void){
 
         if (i % 10 == 0)
             printf("\n");
-        // write(1, &line[i], i);
         
     }
     printf("\n");
-    // printf("finally: %s\n", line);
 
     tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-
-
     return 0;
 }
 
