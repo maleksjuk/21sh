f03fad1d2654f2a4f266f4bd53bf1b4e2c4e9d22
Vanisher Daemoniac <vdaemoni@ga-p3.msk.21-school.ru>
2020-11-21 14:12:41 +0300

    signal norm

--- srcs/signal.c
+++ srcs/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: obanshee <obanshee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vdaemoni <vdaemoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 11:48:07 by obanshee          #+#    #+#             */
-/*   Updated: 2020/11/18 19:54:10 by obanshee         ###   ########.fr       */
+/*   Updated: 2020/11/21 14:10:25 by vdaemoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,9 @@ void	cmd_listener(int sig)
 	if (sig == SIGINT)
 	{
 		ft_printf("Here!");
-		//kill(g_pid, sig);
 		ft_printf("\n");
 	}
 }
-
-/*void	cmd_listener_chld(int sig)
-{
-	if (sig == SIGINT)
-	{
-		ft_printf("");
-		ft_printf("\n");
-		signal(SIGINT, SIG_DFL);
-	}
-}*/
 
 void	main_listener(int sig)
 {
@@ -44,7 +33,6 @@ void	main_listener(int sig)
 void	signals(void)
 {
 	signal(SIGINT, main_listener);
-	//signal(SIGINT, cmd_listener);
-	// signal(SIGTSTP, SIG_IGN);
-	// signal(SIGQUIT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 }
